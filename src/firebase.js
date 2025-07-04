4298e0383e48f2e65beec9b469633b70a0035578
sai suraj <saisuraj1537@gmail.com>
2025-07-04 22:51:55 +0530

    Your message

--- src/firebase.js
+++ src/firebase.js
@@ -3,21 +3,18 @@ import { initializeApp } from 'firebase/app';
 import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAgl8cOdZHmI51AkuqOVaSBcZENXzT1aUg",
-  authDomain: "vijay-caterers.firebaseapp.com",
-  databaseURL: "https://vijay-caterers-default-rtdb.firebaseio.com",
-  projectId: "vijay-caterers",
-  storageBucket: "vijay-caterers.firebasestorage.app",
-  messagingSenderId: "151503284772",
-  appId: "1:151503284772:web:ad1c8aac2ba87de0a6c6b1",
-  measurementId: "G-SQZERF7VKH"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Get Realtime Database instance
 const database = getDatabase(app);
 
-// Export both
 export { app, database };
